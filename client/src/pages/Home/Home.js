d3c2c84dcde2368223dd61211e998d94d4f44f07
Ayman Omer <aymangebrilomer@gmail.com>
2021-04-27 15:33:45 -0400

    i add pages

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>this is the home page</div>;
+};
+
+export default Home;
---
+++ client/src/pages/SingUp/SingUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingUp = () => {
+  return <div>this is Sinup page </div>;
+};
+
+export default SingUp;
