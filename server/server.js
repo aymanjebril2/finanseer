641469047aba43d039d8ec12329bb2489aa4c6a5
Spencer Carver <spencer.carver@gmail.com>
2021-04-27 21:18:39 -0400

    add express + cors for server with a simple endpoint

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
