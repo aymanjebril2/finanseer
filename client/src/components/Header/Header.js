5bec0588292191d62c5ef6795bfc69a416b5d92e
Ayman Omer <aymangebrilomer@gmail.com>
2021-04-29 01:16:30 -0400

    add the loogo crystal-ball and i user profile img it jus display there first letter from there name

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,26 +2,40 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import logo from "./img/crystal-ball.png";
+import Avatar from "@material-ui/core/Avatar";
+import "./Header.css";
+import { deepOrange } from "@material-ui/core/colors";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme) =>
   createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
+    orange: {
+      color: theme.palette.getContrastText(deepOrange[500]),
+      backgroundColor: "#691b99",
+
+      height: 50,
+      width: 50,
+      border: "3px solid plum",
     },
   })
 );
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <AppBar style={{ backgroundColor: "rgba(121,9,113,1)" }}>
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <div className="logo_div">
+            <img src={logo} alt="fainaseer logo" className="logo_img" />
+          </div>
+          <div>
+            <Avatar alt="" src="" className={classes.orange}>
+              A
+            </Avatar>
+          </div>
+        </Toolbar>
       </AppBar>
     </div>
   );
