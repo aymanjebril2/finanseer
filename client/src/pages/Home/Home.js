2e89ad02328de093f85ca064cdfdc7b41e4ffe18
Ayman Omer <aymangebrilomer@gmail.com>
2021-04-30 16:16:05 -0400

    able to add transction senction you can add and delete trnasction

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Grid } from "@material-ui/core";
 import Details from "../../components/Details/Details";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import "./Home.css";
+import useStyles from "./styles";
 import Main from "../../components/Main/Main";
 
 const Home = () => {
+  const classes = useStyles();
+  const main = useRef(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -14,22 +17,25 @@ const Home = () => {
     }, 1000);
   }, []);
   return (
-    <div>
+    <div className="root_div_home">
       <Grid
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
         justify="center"
         style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
