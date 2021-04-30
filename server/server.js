cc6a409fbc32ed8d08f8041347011c8eb01a99ec
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-04-30 19:54:26 -0400

    Added content to Server.js

--- server/server.js
+++ server/server.js
@@ -4,22 +4,21 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+app.use("/api/finance", finance());
+app.use("/api", authentication());
+
+app.use("*", (request, response) => {
+  response.status(404).json({ message: "api not found" });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
